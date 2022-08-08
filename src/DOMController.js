18fb9938bc238aa778459c502876b475649f7abf
Mrljsh <dragan.mrljes@vsar.edu.rs>
2022-08-08 13:44:37 +0200

    Added get and set active project, both in logic and DOM

--- src/DOMController.js
+++ src/DOMController.js
@@ -1,16 +1,20 @@
-import { createProject, getProjects } from "./projectLogic";
+import { createProject, getActiveProjectIndex, getProjects, setActiveProject } from "./projectLogic";
 
 const listSelector = document.querySelector(".folderLists");
 
 const projectsList = getProjects();
+
 
 export function renderProjects() {
     listSelector.replaceChildren();
     projectsList.forEach(project => {
         const listItem = document.createElement('p');
         listItem.textContent = project.name;
+        listItem.dataset.index = projectsList.indexOf(project);
         listSelector.append(listItem);
     });
+    setActiveClass();
+    setProjectClickListeners();
 };
 
 let addNewFolder = (() => {
@@ -37,4 +41,25 @@ let addNewFolder = (() => {
     });
 })();
 
-//RENDER
+function setActiveClass() {
+    const activeFolderIndex = getActiveProjectIndex();
+    const folderList = document.querySelectorAll('.folderLists > *');
+
+    folderList[activeFolderIndex].classList.add('active');
+
+}
+
+function setProjectClickListeners() {
+    const folderList = document.querySelectorAll('.folderLists > *');
+
+    folderList.forEach(folder => {
+        folder.addEventListener('click', () => {
+            setActiveProject(folder.dataset.index);
+            renderProjects();
+        })
+    });
+};
+
+//RENDER PROJECT TAB
+//HOW TO KNOW WHICH ONE IS ACTIVE? (ID's, name??, index?)
+//CREATE MODAL FOR NEW TASK
--- src/projectLogic.js
+++ src/projectLogic.js
@@ -1,12 +1,38 @@
-let projects = []
+let projects = [{name:"TEST PROJECT", toDoList: [], active: true}, ]
 
 export const createProject = (name) => {
+    _clearActiveProjects();
     projects.push({
         name,
         toDoList: [],
+        active: true,
     });
 };
 
 export const getProjects = () => {
     return projects;
 };
+
+function _clearActiveProjects(){
+    projects.forEach(project => project.active = false);
+}
+
+function getActiveProject(){
+    let activeProject = projects.filter(project => project.active === true);
+    return activeProject;
+}
+
+function getActiveProjectIndex(){
+    let activeProject = projects.findIndex(project => project.active === true);
+    return activeProject;
+}
+
+function setActiveProject(index){
+    _clearActiveProjects();
+    projects[index].active = true;
+    console.log(projects[index].active);
+}
+
+
+export { getActiveProjectIndex, setActiveProject }
+//CREATE TODO LOGIC
